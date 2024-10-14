d9cf83b434acc907c05c26461fe891db21579f73
DoHoonKim8 <dohoon1097819@gmail.com>
2024-10-14 08:14:46 +0000

    Update basic numeric ops

--- numeric.hpp
+++ numeric.hpp
@@ -3,19 +3,14 @@
 namespace numeric {
     using uint128_t = unsigned __int128;
     constexpr uint64_t addc(const uint64_t a, const uint64_t b, const uint64_t carry_in, uint64_t& carry_out) noexcept {
-        uint64_t r = a + b;
-        const uint64_t carry_temp = r < a;
-        r += carry_in;
-        carry_out = carry_temp + (r < carry_in);
-        return r;
+        uint128_t res = static_cast<uint128_t>(a) + static_cast<uint128_t>(b) + static_cast<uint128_t>(carry_in);
+        carry_out = static_cast<uint64_t>(res >> 64);
+        return static_cast<uint64_t>(res);
     }
 
     constexpr uint64_t sbb(const uint64_t a, const uint64_t b, const uint64_t borrow_in, uint64_t& borrow_out) noexcept {
-        uint64_t t_1 = a - (borrow_in >> 63ULL);
-        uint64_t borrow_temp_1 = t_1 > a;
-        uint64_t t_2 = t_1 - b;
-        uint64_t borrow_temp_2 = t_2 > t_1;
-        borrow_out = 0ULL - (borrow_temp_1 | borrow_temp_2);
-        return t_2;
+        uint128_t res = static_cast<uint128_t>(a) - (static_cast<uint128_t>(b) + static_cast<uint128_t>(borrow_in >> 63));
+        borrow_out = static_cast<uint64_t>(res >> 64);
+        return static_cast<uint64_t>(res);
     }
 }
